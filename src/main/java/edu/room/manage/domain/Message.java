dd1d05816d797955fded79e29b266c6a12684c1a
执笔 <805073259@qq.com>
2019-05-16 22:34:42 +0800

    1

---
+++ src/main/java/edu/room/manage/domain/Message.java
@@ -0,0 +1,25 @@
+package edu.room.manage.domain;
+
+import edu.room.manage.common.base.dto.BaseDomain;
+import lombok.Data;
+
+import javax.persistence.Table;
+
+/**
+ * @author 执笔
+ * @date 2019/5/16 22:09
+ */
+@Data
+@Table(name = "message")
+public class Message  extends BaseDomain {
+
+    /**
+     *
+     */
+    private Integer userId;
+
+    /**
+     * 内容
+     */
+    private String content;
+}
